4146b7f7506f5cfa2495ade7d57ee21ddd95b288
denmigda <denis.migdal@uca.fr>
2026-07-04 20:15:00 +0200

    minor change

--- src/components/frames/FrameUCATitle/index.ts
+++ src/components/frames/FrameUCATitle/index.ts
@@ -20,7 +20,7 @@ const FrameUCATitle = defineWebComponent(null, {
         const modifDate = new Date(document.lastModified).toLocaleDateString('fr-FR', { year:"numeric", month:"long", day:"numeric", hour:"2-digit", minute: "2-digit"});
 
         // we use standard attr attributes.
-        ctx.elements.title   .textContent    = ctx.target.getAttribute("title");
+        ctx.elements.title   .textContent = ctx.target.getAttribute("title");
         ctx.elements.subtitle.textContent = ctx.target.getAttribute("subtitle");
         ctx.elements.author  .textContent = ctx.target.getAttribute("author");
         ctx.elements.mail    .textContent = ctx.target.getAttribute("mail");
