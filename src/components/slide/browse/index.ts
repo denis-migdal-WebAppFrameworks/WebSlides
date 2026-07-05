ec659de17f19b7bf0ed3c3aee2b488c050d403a4
denmigda <denis.migdal@uca.fr>
2026-07-05 15:55:07 +0200

    Use AnimationNode for browse

--- src/components/slide/browse/index.ts
+++ src/components/slide/browse/index.ts
@@ -1,73 +1,123 @@
 // change slide
-let current = 0;
 
-function getCode(ev: KeyboardEvent) {
+//TODO: move somewhere...
+interface AnimationNode {
+    readonly stepIndex: number;
+    readonly stepCount: number;
+    setStep(step: number): void;
+}
+
+function prev(target: AnimationNode) {
+    if( target.stepIndex === 0 )
+        return;
+    target.setStep( target.stepIndex-1 );
+}
+function next(target: AnimationNode) {
+    if( target.stepIndex >= target.stepCount - 1 )
+        return;
+    target.setStep( target.stepIndex+1 );
+}
+
+// TODO: setPage...
+class Frames implements AnimationNode {
+
+    readonly frames       : readonly HTMLElement[];
+    readonly framesEndSteps: number[];
+
+    stepIndex: number = 0;
+    readonly stepCount: number;
+
+    constructor(target: HTMLElement) {
+        this.frames = [...target.querySelectorAll<HTMLElement>('.ws-frame')];
+
+        this.framesEndSteps = new Array(this.frames.length);
+        let cur = 0;
+        for(let i = 0; i < this.frames.length; ++i) {
+            //TODO...
+            // @ts-ignore
+            cur += this.frames[i].toto ?? 1;
+            this.framesEndSteps[i] = cur;
+        }
+
+        this.stepCount = cur;
+    }
+
+    setStep(step: number): void {
+
+        const frameID = this.framesEndSteps.findIndex( endStep => step < endStep);
+
+        // avoid : offsetTop doesn't work with subpixels, produces jitters.
+        this.frames[frameID].scrollIntoView({behavior: "instant"});
+
+        //TODO: set frame step...
+
+        this.stepIndex = step;
+    }
+
+    updateCurrentStep() {
+
+        const pos = document.querySelector("main")!.scrollTop;
+
+        let cur = 0;
+        while( cur != this.frames.length - 1 && pos >= this.frames[cur].offsetTop)
+            ++cur;
+
+        console.warn(cur);
+
+        if( cur !== 0 )
+            cur = this.framesEndSteps[cur-1];
+
+        console.warn("end", cur);
+
+        this.setStep(cur);
+    }
+}
+
+const frames = new Frames(document.body);
+
+// restore scroll immediately.
+history.scrollRestoration = "manual";
+document.getElementById(location.hash.slice(1))
+        ?.scrollIntoView({ behavior: "smooth" });
+frames.updateCurrentStep();
+
+window.addEventListener("hashchange", () => {
+    frames.updateCurrentStep();
+});
+
+function getAction(ev: KeyboardEvent) {
 
     let code = ev.code;
 
-    if( code === "ArrowLeft"  || code === "ArrowUp")
-        code = "PageUp";
-    if( code === "ArrowRight" || code === "ArrowDown")
-        code = "PageDown";
+    // PageUp/PageDown is emitted by pointers.
+    if( code == "PageUp" || code === "ArrowLeft"  || code === "ArrowUp")
+        return -1;
+    if( code == "PageDown" || code === "ArrowRight" || code === "ArrowDown")
+        return +1;
 
-    return code;
-}
-function isDownUp(code: string) {
-    return ["PageUp", "PageDown"].includes(code);
+    return null;
 }
 
 document.addEventListener("keydown", (ev) => {
 
-    const code = getCode(ev);
+    const code = getAction(ev);
 
-    if( isDownUp( code ) ) {
-        ev.preventDefault();
-        handleEvent( code );
-    }
+    if( code === null)
+        return;
 
+    ev.preventDefault();
+    if( code === -1 )
+        prev(frames);
+    if( code === +1 )
+        next(frames);
 });
 
 document.addEventListener("keypress", (ev) => {
-
-    if( isDownUp( getCode(ev) ) )
+    if( getAction(ev) !== null )
         ev.preventDefault();
 });
 
 document.addEventListener("keyup", (ev) => {
-
-    if( isDownUp( getCode(ev) ) )
+    if( getAction(ev) !== null )
         ev.preventDefault();
 });
-
-function handleEvent(code: string) {
-
-    let sections = [...document.querySelectorAll<HTMLElement>('.ws-frame')];
-
-    const pos = document.querySelector("main")!.scrollTop;
-
-    /*
-    if( ev.code=== "ArrowLeft" && current !== 0)
-        --current;
-    if( ev.code=== "ArrowRight" && current !== sections.length - 1 )
-        ++current;
-    */
-    if( code=== "PageUp" ) {
-
-        let cur = sections.length - 1;
-        while( cur != 0 && pos <= sections[cur].offsetTop)
-            --cur;
-
-        current = cur;
-    }
-    if( code=== "PageDown" ) {
-
-        let cur = 0;
-        while( cur != sections.length - 1 && pos >= sections[cur].offsetTop)
-            ++cur;
-
-        current = cur;
-    }
-
-    // avoid : offsetTop doesn't work with subpixels, produces jitters.
-    sections[current].scrollIntoView({behavior: "instant"});
-}
--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,6 +1,5 @@
 import "WebSlides@2026:components/slide/overview";
 import "WebSlides@2026:components/slide/scale";
-import "WebSlides@2026:components/slide/browse";
 import "WebSlides@2026:components/slide/menu";
 
 import "WebSlides@2026:components/frames/FrameUCA";
@@ -9,6 +8,9 @@ import "WebSlides@2026:components/frames/FrameUCAPlain";
 
 import "MWL@2026:Components/page/color-switch";
 import "MWL@2026:Components/code/code-script";
+
+// we need the slides to be declared before being able to browse.
+import "WebSlides@2026:components/slide/browse";
 
 /*
 import LISS from "@LISS/src";
