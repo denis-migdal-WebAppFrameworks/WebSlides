da7d820c38c13678b887a1db6b639652556e735c
denmigda <denis.migdal@uca.fr>
2026-09-19 09:57:34 +0200

    use @ for special page dirs

--- src/widgets/frames/FrameUCA/index.ts
+++ src/widgets/frames/FrameUCA/index.ts
@@ -1,5 +1,5 @@
 import { View, LazyCoordinator, defineWidget } from "MWL@2026/exports/Widget";
-import { listen, observe } from "MWL@2026/exports/Reactive/Observable";
+import { observe } from "MWL@2026/exports/Reactive/Observable";
 import { frameEffect } from "MWL@2026/exports/browser/scheduler";
 import { ReactiveAnimationNode } from "WebSlides@2026/models/ReactiveAnimationNode";
 import { VisibilityController } from "WebSlides@2026/presentation/navigator/VisibilityController";
